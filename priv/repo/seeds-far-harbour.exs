920645ccd53030ab31e19a6e0e569a692009ddef
Kirill Korolyov <kirill.korolyov@gmail.com>
2018-02-02 23:29:48 +0000

    Add sees for Far Harbour

---
+++ priv/repo/seeds-far-harbour.exs
@@ -0,0 +1,42 @@
+alias Fallout_4Tracker.Repo
+alias Fallout_4Tracker.Quest
+
+Repo.insert!(%Quest{name: "Far from Home", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Walk in the Park", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Where You Belong", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Best Left Forgotten", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "The Way Life Should Be", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Cleansing the Land", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Reformation", category: "far_harbour_main", status: "not_started"})
+Repo.insert!(%Quest{name: "Close to Home", category: "far_harbour_main", status: "not_started"})
+
+Repo.insert!(%Quest{name: "Brain Dead", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Shipbreaker", category: "far_harbour_side", status: "not_started"})
+
+Repo.insert!(%Quest{name: "Acadian Ideals", category: "far_harbour_arcadia", status: "not_started"})
+Repo.insert!(%Quest{name: "Data Recovery", category: "far_harbour_arcadia", status: "not_started"})
+Repo.insert!(%Quest{name: "The Arrival", category: "far_harbour_arcadia", status: "not_started"})
+Repo.insert!(%Quest{name: "The Price of Memory", category: "far_harbour_arcadia", status: "not_started"})
+Repo.insert!(%Quest{name: "Hunting the Hunter", category: "far_harbour_arcadia", status: "not_started"})
+
+Repo.insert!(%Quest{name: "Ablutions", category: "far_harbour_children", status: "not_started"})
+Repo.insert!(%Quest{name: "The Heretic", category: "far_harbour_children", status: "not_started"})
+Repo.insert!(%Quest{name: "Visions in the Fog", category: "far_harbour_children", status: "not_started"})
+Repo.insert!(%Quest{name: "What Atom Requires", category: "far_harbour_children", status: "not_started"})
+Repo.insert!(%Quest{name: "The Trial of Brother Devin", category: "far_harbour_children", status: "not_started"})
+Repo.insert!(%Quest{name: "Witch Hunt", category: "far_harbour_children", status: "not_started"})
+
+Repo.insert!(%Quest{name: "Blood Tide", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Hull Breach", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Hull Breach 2", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Hull Breach 3", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Living on the Edge", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Rite of Passage", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Safe Passage", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "The Changing Tide", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "The Great Hunt", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "The Hold Out", category: "far_harbour_side", status: "not_started"})
+Repo.insert!(%Quest{name: "Turn Back the Fog", category: "far_harbour_side", status: "not_started"})
+
+Repo.insert!(%Quest{name: "Forbidden Knowledge", category: "far_harbour_commonwealth", status: "not_started"})
+Repo.insert!(%Quest{name: "Search and Destroy", category: "far_harbour_commonwealth", status: "not_started"})
